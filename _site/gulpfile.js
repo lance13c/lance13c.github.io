f34f4f9fc382bde92d8e34c1614f72386fef2548
lance13c <pianoman13c@gmail.com>
2016-09-11 02:31:02 -0400

    add _site

---
+++ _site/gulpfile.js
@@ -0,0 +1,51 @@
+
+const gulp = require('gulp');
+const usemin = require('gulp-usemin');
+const uglify = require('gulp-uglify');
+const minifyCss = require('gulp-minify-css');
+const replace = require('gulp-replace');
+
+const paths = {
+	fonts: './node_modules/materialize-css/dist/font/**/*.*',
+	css: './_includes/build/css/*.css',
+	js: './_includes/build/js/*.js',
+	build: './_includes/build',
+	pre_base: './_dependencies/base.html'
+};
+
+gulp.task('usemin', () => {
+	return gulp.src(paths.pre_base)
+		.pipe(usemin({
+			css: [],
+			js: []
+		}))
+		.pipe(gulp.dest(paths.build));
+});
+
+gulp.task('replace-hrefs', ['usemin'], () => {
+	return gulp.src(`${paths.build}/link.html`)
+		.pipe(gulp.dest('./_includes/build'));
+});
+
+gulp.task('copy-base', ['usemin'], () => {
+	return gulp.src(`${paths.build}/base.html`)
+		.pipe(gulp.dest('./_layouts/'));
+});
+
+gulp.task('copy-css', ['usemin'], () => {
+	return gulp.src(paths.css)
+		.pipe(gulp.dest('./_site/css'));
+});
+
+gulp.task('copy-js', ['usemin'], () => {
+	return gulp.src(paths.js)
+		.pipe(gulp.dest('./_site/js'));
+});
+
+gulp.task('copy-fonts', () => {
+	return gulp.src(paths.fonts)
+		.pipe(gulp.dest('./_site/font'));
+});
+
+
+gulp.task('default', ['usemin', 'copy-base', 'copy-css', 'copy-js', 'copy-fonts']);
---
+++ _site/js/index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Lance on 9/5/2016.
+ */
+
+
+(() => {
+	
+	// Constants
+	const navHeight = 63; //px
+	
+	// Changes the body to full height & width
+	function resize() {
+		$("body").css('height', $(window).height());
+		$("body").css('width', $(window).width());
+		$(".cs-content").css('height', $(window).height() - navHeight);
+	}
+	resize();
+	
+	// Triggers resize on every screen size change
+	$(window).on("resize onorientationchange", (e) => {
+		resize();
+	});
+	
+})();
+
+
