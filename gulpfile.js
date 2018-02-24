c3972bc15f3cd0515336fe596fcd6484741caa6e
lance13c <pianoman13c@gmail.com>
2018-02-23 22:27:57 -0500

    added partials 7-1 folders

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
  
+const paths = {
+  styles: {
+    src: './styles/sass/partials',
+    main: './styles/sass/main.scss',
+    files: './styles/sass/**/*.scss',
+    dest: './styles/css'
+  }
+}
+
 gulp.task('sass', function () {
-  return gulp.src('./styles/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./styles/css'));
+  return gulp.src(paths.styles.main)
+    .pipe(sass({
+        outputStyle: 'compressed',
+        sourceComments: 'map',
+        includePaths : [paths.styles.src]
+      }
+    ).on('error', sass.logError))
+    .pipe(gulp.dest(paths.styles.dest));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./styles/**/*.scss', ['sass']);
+  gulp.watch(paths.styles.files, ['sass']);
 });
 
 gulp.task('default', ["sass", "sass:watch"]);
