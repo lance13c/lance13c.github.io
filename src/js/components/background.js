f07dd45b8ba3bbf55783c878c9521dbc2ce8935e
lance13c <pianoman13c@gmail.com>
2018-02-28 16:50:22 -0500

    update background

--- src/js/components/background.js
+++ src/js/components/background.js
@@ -8,66 +8,72 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 
-// Default Vectors
+// Used in init
+let clock;
 
+// Used in initParticles
+let floorGroup;
+
+// Default Vectors
 // Position based on field
 let posMiddle = new THREE.Vector3(500, 0, 500);
-
 // Rotation
-
-
 // Objects
-
-
-
 // Floor Texture
 
-let floorEmitter = new SPE.Emitter({
-    type: SPE.distributions.SPHERE,
-    particleCount: 10000,
-    isStatic: true,
-    maxAge: {
-        value: 8,
-        spread: 3
-    },
-    position: {
-        value: new THREE.Vector3(0,0,0),
-        radius: 10
-    },
-    wiggle: {
-        value: 1,
-        spread: 0.1
-    },
-    color: {
-        //value: new THREE.Color(abstracts.themeMonoColor4)
-    },
-    size: {
-        value: 1
-    },
-    angle: {
-        value: .1,
-        spread: .2
-    }
-});
+// Init
+function init() {
+    clock = new THREE.Clock();
+}
 
-let floorGroup = new SPE.Group({
-	texture: {
-        value: new THREE.TextureLoader().load( "/assets/images/textures/grass_blade.png")
-    },
-    maxParticleCount: 10000,
-    hasPerspective: false,
-    colorize: false,
-    blending: THREE.AdditiveBlending,
-    transparent: false,
-    alphaTest: 5,
-    depthWrite: 0,
-    fog: 1,
-    scale: 1
-});
+function initParticles() {
+    floorGroup = new SPE.Group({
+        texture: {
+            value: new THREE.TextureLoader().load( "/assets/images/textures/grass_blade.png")
+        },
+        maxParticleCount: 10000,
+        // hasPerspective: false,
+        // colorize: false,
+        // blending: THREE.AdditiveBlending,
+        // transparent: false,
+        // alphaTest: 5,
+        // depthWrite: 0,
+        // fog: 1,
+        // scale: 1
+    });
+    
+    let floorEmitter = new SPE.Emitter({
+        type: SPE.distributions.SPHERE,
+        particleCount: 10000,
+        isStatic: true,
+        maxAge: {
+            value: 8,
+            spread: 3
+        },
+        position: {
+            value: new THREE.Vector3(0,0,0)
+        },
+        wiggle: {
+            value: 1,
+            spread: 0.1
+        },
+        color: {
+            value: new THREE.Color(abstracts.accentColor)
+        },
+        size: {
+            value: 10
+        },
+        angle: {
+            value: .1,
+            spread: .2
+        }
+    });
+    
+    
+    floorGroup.addEmitter(floorEmitter);
+    scene.add(floorGroup.mesh);
+}
 
-floorGroup.addEmitter(floorEmitter);
-
-scene.add(floorGroup.mesh);
 
 // Floor
 let floorG = new THREE.BoxGeometry( 1000, 0.1, 1000 );
@@ -81,11 +87,11 @@ let floorM = new THREE.MeshStandardMaterial( {
 // scene.add( floorMesh );
 
 
-// let farWallG = new THREE.BoxGeometry( 10000, 10000, 0.1);
-// let farWallM = new THREE.MeshStandardMaterial( { color: abstracts.themeMonoColor1 } );
-// let farWallMesh = new THREE.Mesh( farWallG, farWallM );
-// farWallMesh.translateZ(-500);
-// scene.add( farWallMesh );
+let farWallG = new THREE.BoxGeometry( 10000, 10000, 0.1);
+let farWallM = new THREE.MeshStandardMaterial( { color: abstracts.themeMonoColor1 } );
+let farWallMesh = new THREE.Mesh( farWallG, farWallM );
+farWallMesh.translateZ(-500);
+scene.add( farWallMesh );
 
 let testOBJG = new THREE.BoxGeometry( 1, 1, 1);
 let testOBJM = new THREE.MeshStandardMaterial( { color: abstracts.accentColor } );
@@ -112,18 +118,31 @@ function particleGroupUpdate( deltaTime ) {
     floorGroup.tick( deltaTime );
 }
 
-// Animate
-function animate(deltaTime) {
+window.addEventListener( 'resize', function() {
+    let w = window.innerWidth,
+        h = window.innerHeight;
+    camera.aspect = w / h;
+    camera.updateProjectionMatrix();
+    renderer.setSize( w, h );
+}, false );
 
-    if (deltaTime) {
-        particleGroupUpdate(deltaTime);
-    }
 
-    //test
-    //camera.rotateY(.01);
-    //camera.rotateX(.01);
-    //floorMesh.rotateX(.1);
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+
+function render(dt) {
+    //camera.rotateY(0.01);
+    //camera.rotateX(0.02);
+    particleGroupUpdate(dt);
+    renderer.render( scene, camera );
 }
+
+// Animate
+function animate() {
+    requestAnimationFrame( animate );
+    render(clock.getDelta());
+}
+
+
+
+init();
+initParticles();
 animate();
