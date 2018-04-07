b6221ae19cbac4e636b1e75aea2d55add587d42f
lance13c <pianoman13c@gmail.com>
2018-04-07 00:28:15 -0400

    css html tests

--- dist/js/main.js
+++ dist/js/main.js
@@ -46691,22 +46691,28 @@ sceneEl.addEventListener('loaded', function () {
 });
 
 },{"./abstracts":3,"./pages/home":5,"./until/nav":6,"three-css3drenderer":1}],5:[function(require,module,exports){
-'use strict';
+"use strict";
 
 function init(cssScene, elements, objects3d) {
     var cssContainerEl = document.createElement('div');
+    cssContainerEl.setAttribute("id", "vr-home");
+
     var cssHeaderEl = document.createElement('h1');
     cssHeaderEl.innerHTML = "testing";
+    cssHeaderEl.setAttribute('class', "header");
 
     var cssTextEl = document.createElement("p");
-    cssTextEl.innerHTML = 'Testing html in webGL Testing html in webGL \n    Testing html in webGL Testing html in webGL\n    Testing html in webGL\n    Testing html in webGL';
+    cssTextEl.innerHTML = "Testing html in webGL Testing html in webGL \n    Testing html in webGL Testing html in webGL\n    Testing html in webGL\n    Testing html in webGL\n    Testing html in webGL\n    Testing html in webGL\n    Testing html in webGL\n    Testing html in webGL\n    Testing html in webGL\n    \n    ";
 
     cssContainerEl.appendChild(cssHeaderEl);
     cssContainerEl.appendChild(cssTextEl);
     // create the object3d for this element
     var cssObject = new THREE.CSS3DObject(cssContainerEl);
     // we reference the same position and rotation 
-    cssObject.position.set(objects3d.homePanel.position.x, objects3d.homePanel.position.y, objects3d.homePanel.position.z);
+
+
+    //const yOffset = 0.55;
+    cssObject.position.set(objects3d.homePanel.position.x, 0, objects3d.homePanel.position.z);
     cssObject.scale.set(0.01, 0.01, 0.01);
     cssObject.rotation.set(0, 0, 0);
     // add it to the css scene
--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,21 +1,34 @@
 
 function init(cssScene, elements, objects3d) {
     let cssContainerEl = document.createElement('div');
+    cssContainerEl.setAttribute("id", "vr-home");
+
     let cssHeaderEl = document.createElement('h1');
     cssHeaderEl.innerHTML = "testing";
+    cssHeaderEl.setAttribute('class', "header");
 
     let cssTextEl = document.createElement("p");
     cssTextEl.innerHTML =`Testing html in webGL Testing html in webGL 
     Testing html in webGL Testing html in webGL
     Testing html in webGL
-    Testing html in webGL`;
+    Testing html in webGL
+    Testing html in webGL
+    Testing html in webGL
+    Testing html in webGL
+    Testing html in webGL
+    Testing html in webGL
+    
+    `;
 
     cssContainerEl.appendChild(cssHeaderEl);
     cssContainerEl.appendChild(cssTextEl);
     // create the object3d for this element
     var cssObject = new THREE.CSS3DObject( cssContainerEl );
     // we reference the same position and rotation 
-    cssObject.position.set(objects3d.homePanel.position.x, objects3d.homePanel.position.y, objects3d.homePanel.position.z);
+
+
+    //const yOffset = 0.55;
+    cssObject.position.set(objects3d.homePanel.position.x, 0, objects3d.homePanel.position.z);
     cssObject.scale.set(0.01, 0.01, 0.01);
     cssObject.rotation.set(0, 0, 0);
     // add it to the css scene
