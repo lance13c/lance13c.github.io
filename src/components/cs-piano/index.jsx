3de0b152d950eee1fbeba95457813a5264fcf8dd
lance13c <pianoman13c@gmail.com>
2019-10-05 06:13:16 -0400

    WIP piano with synth sounds

--- src/components/cs-piano/index.jsx
+++ src/components/cs-piano/index.jsx
@@ -1,25 +1,89 @@
 import React, { Component } from 'react';
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
-import SoundfontProvider from './soundfont-provider';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
 import 'react-piano/dist/styles.css';
 
+import './index.scss';
+
 import Tone from 'tone';
+
+const midiToKeyMap = new Map([
+	[ '96', 'C7' ],
+	[ '95', 'B6' ],
+	[ '94', 'Bb6' ],
+	[ '93', 'A6' ],
+	[ '92', 'Ab6' ],
+	[ '91', 'G6' ],
+	[ '90', 'Gb6' ],
+	[ '89', 'F6' ],
+	[ '88', 'E6' ],
+	[ '87', 'Eb6' ],
+	[ '86', 'D6' ],
+	[ '85', 'Db6' ],
+	[ '84', 'C6' ],
+	[ '83', 'B5' ],
+	[ '82', 'Bb5' ],
+	[ '81', 'A5' ],
+	[ '80', 'Ab5' ],
+	[ '79', 'G5' ],
+	[ '78', 'Gb5' ],
+	[ '77', 'F5' ],
+	[ '76', 'E5' ],
+	[ '75', 'Eb5' ],
+	[ '74', 'D5' ],
+	[ '73', 'Db5' ],
+	[ '72', 'C5' ],
+	[ '71', 'B5' ]
+]);
 
 class CSPiano extends Component {
 	constructor(props) {
 		super(props);
 
-		// webkitAudioContext fallback needed to support Safari
-		this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
-		this.soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
+		this.state = {
+			attack: 0.02,
+			decay: 0.1,
+			sustain: 0.2,
+			release: 0.9
+		};
 
-		this.synth = new Tone.Synth().toMaster();
+		this.handleOnAttackChange = this.handleOnAttackChange.bind(this);
+		this.synth = new Tone.AMSynth({
+			harmonicity: 2.2,
+			detune: 0,
+			oscillator: {
+				type: 'sine'
+			},
+			envelope: {
+				attack: 0.0001,
+				decay: 1,
+				sustain: 0.2,
+				release: 0.3
+			},
+			modulation: {
+				type: 'sine'
+			},
+			modulationEnvelope: {
+				attack: 0.001,
+				decay: 0,
+				sustain: 1,
+				release: 0.5
+			}
+		}).toMaster();
 	}
 
-	state = {};
+	handleOnAttackChange(e, val) {
+		this.setState((preState) => {
+			return Object.assign(preState, {
+				attack: val
+			});
+		});
+	}
+
 	render() {
-		const firstNote = MidiNumbers.fromNote('c3');
-		const lastNote = MidiNumbers.fromNote('f5');
+		const firstNote = MidiNumbers.fromNote('c5');
+		const lastNote = MidiNumbers.fromNote('c7');
 		const keyboardShortcuts = KeyboardShortcuts.create({
 			firstNote: firstNote,
 			lastNote: lastNote,
@@ -27,21 +91,43 @@ class CSPiano extends Component {
 		});
 
 		return (
-			<SoundfontProvider
-				instrumentName="acoustic_grand_piano"
-				audioContext={this.audioContext}
-				hostname={this.soundfontHostname}
-				render={({ isLoading, playNote, stopNote }) => (
-					<Piano
-						noteRange={{ first: firstNote, last: lastNote }}
-						playNote={playNote}
-						stopNote={stopNote}
-						width={1000}
-						disabled={isLoading}
-						keyboardShortcuts={keyboardShortcuts}
+			<div className="cs-piano_wrapper">
+				<Piano
+					noteRange={{ first: firstNote, last: lastNote }}
+					playNote={(midiNumber) => {
+						// Play a given note - see notes below
+						const note = midiToKeyMap.get(String(midiNumber));
+						console.log(midiNumber, note);
+						this.synth.triggerAttack(note, '4n');
+
+						// this.synth.triggerAttack(midiNumber, '8n');
+					}}
+					stopNote={(midiNumber) => {
+						// Stop playing a given note - see notes below
+						console.log(midiNumber);
+						this.synth.triggerRelease();
+					}}
+					width={1000}
+					keyboardShortcuts={keyboardShortcuts}
+				/>
+				<div>
+					<Typography id="discrete-slider" gutterBottom>
+						Attack
+					</Typography>
+
+					<Slider
+						className="cs-slider"
+						defaultValue={this.state.attack}
+						aria-labelledby="discrete-slider"
+						valueLabelDisplay="auto"
+						onChange={this.handleOnAttackChange}
+						step={0.01}
+						marks
+						min={0.01}
+						max={1}
 					/>
-				)}
-			/>
+				</div>
+			</div>
 		);
 	}
 }
